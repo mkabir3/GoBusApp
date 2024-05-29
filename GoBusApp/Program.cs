102a9c00dd140509c107fdf9153c9439e96449fb
unknown <humayun418@yahoo.com>
2024-05-28 23:55:25 -0400

    Bringing into github

---
+++ GoBusApp/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoBusApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public class Commuter
+        {
+            private int commID;
+            private string commName;
+            private string commLocation;
+            private string email;
+
+            public void CreateCommuter()
+
+            {   //TODO- implement Commuter.CreateCommuter
+                throw new UnsupportedOperationException();
+            }
+
+            public void loginAccount()
+
+            {   
+                throw new UnsupportedOperationException();
+            }
+
+            public void viewDisplayLocation()
+            {
+                throw new UnsupportedOperationException();
+            }
+
+            public void viewDisplayArrivalTime()
+            {
+                throw new UnsupportedOperationException();
+            }
+
+            public void viewDisplayRoute()
+            {
+                throw new UnsupportedOperationException();
+            }
+
+            public void viewDisplayNotification()
+            {
+                throw new UnsupportedOperationException();
+            }
+
+            public void addComments()
+            {
+                throw new UnsupportedOperationException();
+            }
+
+            public void viewRatings()
+            {
+                throw new UnsupportedOperationException();
+            }
+        }
+
+            public class Service
+        {
+            private int busNo;
+            private int routeID;
+            private string busLocation;
+            private int mapID;
+            private string longitude;
+            private string latitude;
+            private int commtID;
+
+            public void loginAccount()
+            {
+                throw new UnsupportedOperationException();
+            }
+
+            public void viewDisplayBusLocation()
+            {
+                throw new UnsupportedOperationException();
+            }
+
+            public void viewDisplayArrivalTime()
+            {
+                throw new UnsupportedOperationException();
+            }
+
+            public void viewDisplayRoute()
+            {
+                throw new UnsupportedOperationException();
+            }
+
+            public void viewDisplayNitification()
+            {
+                throw new UnsupportedOperationException();
+            }
+
+        }
+
+    }
+}
